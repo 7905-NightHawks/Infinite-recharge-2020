06371e3baaf8636ef9703fde4b53c3e4f43b4f2c
7905-BlackHawks <isaiahbailey02@gmail.com>
2020-02-12 17:57:14 -0700

    update

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,16 +26,16 @@ public final class Constants {
         public static final int kRightMotor1Port = 3;
         public static final int kRightMotor2Port = 12;
     
-       public static final int[] kLeftEncoderPorts = new int[]{0, 1};
-        public static final int[] kRightEncoderPorts = new int[]{2, 3};
-        public static final boolean kLeftEncoderReversed = false;
-        public static final boolean kRightEncoderReversed = true;
+       //public static final int[] kLeftEncoderPorts = new int[]{0, 1};
+        //public static final int[] kRightEncoderPorts = new int[]{2, 3};
+       // public static final boolean kLeftEncoderReversed = false;
+        //public static final boolean kRightEncoderReversed = true;
     
-       public static final int kEncoderCPR = 1024 ;
-        public static final double kWheelDiameterInches = 6 ;
-       public static final double kEncoderDistancePerPulse = 
+       //public static final int kEncoderCPR = 1024 ;
+        //public static final double kWheelDiameterInches = 6 ;
+     //  public static final double kEncoderDistancePerPulse = 
             
-            (kWheelDiameterInches * Math.PI) / (double) kEncoderCPR;
+          //  (kWheelDiameterInches * Math.PI) / (double) kEncoderCPR;
     
         public static final boolean kGyroReversed = false;
     
